6ad856d74bc649130748feb810eb6720ef4947a1
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2021-06-25 10:57:12 +1000

    remove arrowheads

--- plot_schematic_diagram.py
+++ plot_schematic_diagram.py
@@ -157,36 +157,6 @@ def add_axes_line(ax, xticks, yticks, xmin_ticks, ymin_ticks):
         ax.plot([-0.025, 0.025], [y, y], **line_kwargs)
 
 
-def add_arrows_to_axes(fig, ax):
-    xmin, xmax = ax.get_xlim()
-    ymin, ymax = ax.get_ylim()
-
-    # get width and height of axes object to compute
-    # matching arrowhead length and width
-    dps = fig.dpi_scale_trans.inverted()
-    bbox = ax.get_window_extent().transformed(dps)
-    width, height = bbox.width, bbox.height
-
-    # manual arrowhead width and length
-    hw = 1. / 20. * (ymax - ymin)
-    hl = 1. / 20. * (xmax - xmin)
-    lw = 1.  # axis line width
-    ohg = 0.3  # arrow overhang
-
-    # compute matching arrowhead length and width
-    yhw = hw / (ymax - ymin) * (xmax - xmin) * height / width
-    yhl = hl / (xmax - xmin) * (ymax - ymin) * width / height
-
-    # draw x and y axis
-    ax.arrow(xmin, 0, xmax - xmin, 0., fc='k', ec='k', lw=lw,
-             head_width=hw, head_length=hl, overhang=ohg,
-             length_includes_head=True, clip_on=False)
-
-    ax.arrow(0, ymin, 0., ymax - ymin, fc='k', ec='k', lw=lw,
-             head_width=yhw, head_length=yhl, overhang=ohg,
-             length_includes_head=True, clip_on=False)
-
-
 def main():
     plot()
 
